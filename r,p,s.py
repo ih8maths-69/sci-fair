7e2b23bd2a403d8b2b3d6b4333b63a5bd6eb7fa6
ih8maths-69 <136494400+ih8maths-69@users.noreply.github.com>
2023-10-20 17:08:30 +0530

    Update r,p,s.py

--- r,p,s.py
+++ r,p,s.py
@@ -1,22 +1,23 @@
-options=('rock','paper','scissors')
 import random
+options=['rock','paper','scissors']
 a=True
 while a:
-    move=input('rock,paper or scissors:').lower()
     com=random.choice(options)
-    if move:
-        print(com)
-        
+    move=input('rock,paper or scissors:').lower()
+    if move in options:
+        print('Computer chooses',com)
+        if move=='rock' and com=='paper':
+            print('you lost L')
+        elif move=='paper' and com=='scissors':
+            print('you lost L')
+        elif move=='sciossors' and com=='rock':
+            print('you losty L')
+        elif move==com:
+            print('its a tie')
+        else:
+            print('You won!')
+        again=input('wanna play again? Y/n').lower()
+        if again=='n':
+            a=False
     else:
         print('type rock,paper or scissors')
-    if move=='rock' and com=='paper':
-        print('You lost L')
-    elif move=='paper' and com=='scissors':
-        print('You lost L')
-    elif move=='scissors'and com=='rock':
-        print('you lost L')
-    else:
-        print('You won!')
-    again=input('Wanna play again? type Y/n:').lower()
-    if again=='n':
-        a=False
