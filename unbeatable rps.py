192a470d1e87922f6f966dcc07a11d2db4e51079
ih8maths-69 <136494400+ih8maths-69@users.noreply.github.com>
2023-12-13 18:05:33 +0530

    Delete unbeatable rps.py

--- unbeatable rps.py
+++
@@ -1,9 +0,0 @@
-x=['rock','paper','scissors']
-y=input('type rock,paper or scissors:')
-if y==x[0]:
-    print('paper')
-elif y==x[1]:
-    print('scissors')
-elif y==x[2]:
-    print('rock')
-print('If u wish to defeat me train for another 100 years !')
